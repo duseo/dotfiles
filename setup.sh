3c1cc23c6b20cc0962ec37dbc08f870554bec29f
duseo <duseo@users.noreply.github.com>
2020-10-26 14:05:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 mkdir ~/.vim/undodir
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd dotfiles
+cp ~/.vimrc ~/oldvimrc
+rm -rf ~/.vimrc
+cat .vimrc >> ~/.vimrc
+    
